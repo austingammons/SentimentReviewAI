a21e16a084c86f8c387b626d6f2f94b071404345
austinmckaegammons <austinmckaegammons@outlook.com>
2023-02-23 06:10:38 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using MyMLApp;
+
+Console.WriteLine("Press q to quit");
+
+bool quitFlag = false;
+while (!quitFlag)
+{
+    Console.WriteLine("Write your review here:");
+    string? input;
+    input = Console.ReadLine();
+    if (input == "q")
+    {
+        quitFlag = true;
+    }
+    else
+    {
+        var data = new SentimentModel.ModelInput()
+        {
+            Col0 = input!
+        };
+
+        var result = SentimentModel.Predict(data);
+        var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+        Console.WriteLine($"Sentiment: {sentiment}\n");
+    }
+}
